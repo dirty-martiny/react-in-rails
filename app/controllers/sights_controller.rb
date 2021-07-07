4b38186b724c6736953c2d7a01b58be277a99352
KevinBurgin <Kevinburgin33@gmail.com>
2021-07-07 14:03:36 -0700

    completed create method and request spec

--- app/controllers/sights_controller.rb
+++ app/controllers/sights_controller.rb
@@ -1,2 +1,14 @@
 class SightsController < ApplicationController
+    def create
+        sight = Sight.create(sight_params)
+        if sight.valid?
+            render json: sight
+        else
+            render json: sight.errors, status: :unprocessable_entity
+        end
+    end
+    private
+    def sight_params
+        params.require(:sight).permit(:name, :address1, :city, :zip_code, :state, :country, :phone, :trip_id)
+    end
 end
