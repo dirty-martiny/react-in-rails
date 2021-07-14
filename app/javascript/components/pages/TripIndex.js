913d6be0b1d0ea8184fca65ede28e0b4f38470e4
Shazeen15 <shazzy.sterlin@gmail.com>
2021-07-14 12:01:16 -0700

    fixing reactstrap styling

--- app/javascript/components/pages/TripIndex.js
+++ app/javascript/components/pages/TripIndex.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-} from "reactstrap";
+import { Card, Button, CardTitle, CardText, Row, Col } from "reactstrap";
 
 export default class TripIndex extends Component {
   render() {
@@ -28,9 +20,7 @@ export default class TripIndex extends Component {
                 <Col sm="3" key={trip.id}>
                   <Card body>
                     <CardTitle tag="h5">{trip.trip_name}</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2">
-                      {trip.trip_location}
-                    </CardSubtitle>
+                    <CardText>{trip.trip_location}</CardText>
                     <Link to={`/trips/${trip.id}`}>
                       <Button>More Details</Button>
                     </Link>
