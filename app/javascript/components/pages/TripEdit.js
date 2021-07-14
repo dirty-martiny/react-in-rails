03ff22e800dbf439f2bdcd5a2730f8c13bd21f2e
Vivean28 <vivean.la@gmail.com>
2021-07-13 23:00:07 -0700

    finshed styling the card

--- app/javascript/components/pages/TripEdit.js
+++ app/javascript/components/pages/TripEdit.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label, Card} from "reactstrap";
 import { Redirect } from "react-router-dom";
 
 export class TripEdit extends Component {
@@ -31,51 +31,53 @@ export class TripEdit extends Component {
     return (
       <div className="form-container">
         <h1>Trip Edit Page</h1>
-        <Form>
-          <FormGroup>
-            <Label>Trip's Name</Label>
-            <Input
-              type="text"
-              name="trip_name"
-              onChange={this.handleChange}
-              value={this.state.form.trip_name}
-            />
-          </FormGroup>
-          <br />
-          <FormGroup>
-            <Label>Trip's Location</Label>
-            <Input
-              type="text"
-              name="trip_location"
-              onChange={this.handleChange}
-              value={this.state.form.trip_location}
-            />
-          </FormGroup>
-          <br />
-          <FormGroup>
-            <Label>Date of Trip</Label>
-            <Input
-              type="text"
-              name="trip_date_range"
-              onChange={this.handleChange}
-              value={this.state.form.trip_date_range}
-            />
-          </FormGroup>
-          <br />
-          <FormGroup>
-            <Label>Check here to show your trip to friends</Label>
-            <Input
-              type="checkbox"
-              name="is_public"
-              checked={this.state.form.is_public}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <br />
-          <Button name="submit" color="secondary" onClick={this.handleSubmit}>
-            Update Trip
-          </Button>
-        </Form>
+        <Card>
+          <Form>
+            <FormGroup>
+              <Label>Trip's Name</Label>
+              <Input
+                type="text"
+                name="trip_name"
+                onChange={this.handleChange}
+                value={this.state.form.trip_name}
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <Label>Trip's Location</Label>
+              <Input
+                type="text"
+                name="trip_location"
+                onChange={this.handleChange}
+                value={this.state.form.trip_location}
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <Label>Date of Trip</Label>
+              <Input
+                type="text"
+                name="trip_date_range"
+                onChange={this.handleChange}
+                value={this.state.form.trip_date_range}
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <Label className="text-for-check">Check here to show your trip to friends</Label>
+              <Input
+                type="checkbox"
+                name="is_public"
+                checked={this.state.form.is_public}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <br />
+            <Button name="submit" color="secondary" onClick={this.handleSubmit}>
+              Update Trip
+            </Button>
+          </Form>
+        </Card>
         {this.state.submitted && <Redirect to="/tripsindex" />}
       </div>
     );
